fc192fef932bcb6c96c831d21f9ef9774d6d26dc
Dominduchami <dominikkobinski314@gmail.com>
2022-10-19 22:21:22 +0200

    PrePi: Cleanup

--- PrePi/Pi.c
+++ PrePi/Pi.c
@@ -20,7 +20,6 @@
 #include <Library/IoLib.h>
 #include <Library/MemoryAllocationLib.h>
 #include <Library/PcdLib.h>
-#include <Library/LKEnvLib.h>
 #include <Library/PeCoffGetEntryPointLib.h>
 #include <Library/PerformanceLib.h>
 #include <Library/PrePiHobListPointerLib.h>
@@ -32,32 +31,7 @@
 #include <Library/ArmHvcLib.h>
 #include <Library/ArmSmcLib.h>
 
-#include <Chipset/psci.h>
-
 VOID EFIAPI ProcessLibraryConstructorList(VOID);
-
-STATIC VOID PsciTest(VOID)
-{
-  ARM_HVC_ARGS         StubArgsHvc;
-
-  DEBUG((EFI_D_INFO, "PSCI Test\n"));
-
-  StubArgsHvc.Arg0 = ARM_SMC_ID_PSCI_VERSION;
-  ArmCallHvc(&StubArgsHvc);
-
-  if (StubArgsHvc.Arg0 == ARM_SMC_PSCI_RET_INVALID_PARAMS) {
-    DEBUG((EFI_D_INFO, "PSCI call returned invalid parameters!\n"));
-  }
-
-  else if (StubArgsHvc.Arg0 == ARM_SMC_PSCI_RET_NOT_SUPPORTED) {
-    DEBUG((EFI_D_INFO, "PSCI call returned : not supported!\n"));
-  }
-
-  else {
-    DEBUG((EFI_D_INFO | EFI_D_LOAD, "PSCI - v%d.%d detected\n", 
-          PSCI_VERSION_MAJOR(StubArgsHvc.Arg0), PSCI_VERSION_MINOR(StubArgsHvc.Arg0)));
-  }
-}
 
 STATIC VOID PsciFixupInit(VOID)
 {
@@ -71,46 +45,29 @@ STATIC VOID PsciFixupInit(VOID)
   LowerELSynchronous64PatchOffset = LOWER_EL_SYNC_EXC_64B_PATCH_ADDR;
   LowerELSynchronous32PatchOffset = LOWER_EL_SYNC_EXC_32B_PATCH_ADDR;
 
-  DEBUG((EFI_D_INFO, "\nPSCI Fixup Init\n"));
-
   CopyMem(
       (VOID *)WakeFromPowerGatePatchOffset, WakeFromPowerGatePatchHandler,
       sizeof(WakeFromPowerGatePatchHandler));
-
-  DEBUG((EFI_D_INFO, "First CopyMem done\n"));
 
   CopyMem(
       (VOID *)LowerELSynchronous64PatchOffset, LowerELSynchronous64PatchHandler,
       sizeof(LowerELSynchronous64PatchHandler));
 
-  DEBUG((EFI_D_INFO, "Second CopyMem done\n"));
-
   CopyMem(
       (VOID *)LowerELSynchronous32PatchOffset, LowerELSynchronous32PatchHandler,
       sizeof(LowerELSynchronous32PatchHandler));
-
-  DEBUG((EFI_D_INFO, "Third CopyMem done\n"));
 
   ArmDataSynchronizationBarrier();
   ArmInvalidateDataCache();
   ArmInvalidateInstructionCache();
 
-  DEBUG((EFI_D_INFO, "Barriers done\n"));
-
-  DEBUG((EFI_D_INFO, "Call into the handler to make HCR_EL2.TSC sticky\n"));
   // Call into the handler to make HCR_EL2.TSC sticky
   StubArgsHvc.Arg0 = ARM_SMC_ID_PSCI_VERSION;
   ArmCallHvc(&StubArgsHvc);
 
-  DEBUG((EFI_D_INFO, "HVC call done\n"));
-
   // Well...
-  DEBUG((EFI_D_INFO, "Call SMC\n"));
-
   StubArgsSmc.Arg0 = ARM_SMC_ID_PSCI_VERSION;
   ArmCallSmc(&StubArgsSmc);
-
-  DEBUG((EFI_D_INFO, "SMC call done\n"));
 }
 
 VOID PrePiMain(IN VOID *StackBase, IN UINTN StackSize)
@@ -123,9 +80,6 @@ VOID PrePiMain(IN VOID *StackBase, IN UINTN StackSize)
   UINTN MemorySize       = 0;
   UINTN UefiMemoryBase   = 0;
   UINTN UefiMemorySize   = 0;
-
-  //PSCI test
-  PsciTest();
 
   // PSCI fixup init
   PsciFixupInit();
