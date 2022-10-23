ce4b39dc314487a52570a3f06d64431cedc85bcf
Dominduchami <dominikkobinski314@gmail.com>
2022-10-23 23:14:26 +0200

    CI: Use latest stable branch
    Suggested-by: sonic011gamer

--- Tools/CI/Bootstrapper/Stage1.sh
+++ Tools/CI/Bootstrapper/Stage1.sh
@@ -6,11 +6,11 @@
 # All rights reserved.
 #
 
-# Checkout EDK2 at master and recent commit only
+# Checkout EDK2 at edk2-stable202208 and recent commit only
 echo "Checking out EDK2 workspace"
 
 cd ..
-git clone --single-branch --depth 1 --recurse-submodules --branch master https://github.com/tianocore/edk2
+git clone --single-branch --depth 1 --recurse-submodules --branch edk2-stable202208 https://github.com/tianocore/edk2
 
 # Set a link to EDK2 workspace
 ln -s $(pwd)/Nexus5XPkg $(pwd)/edk2/Nexus5XPkg
