c0e6d5193e5603c1e256568d8ea6d2f6c217549b
Dominduchami <Dominduchami@outlook.com>
2022-10-23 18:02:44 +0200

    Revert "PrePi: Add back the PSCI patch (#3)"
    This reverts commit 3bb1c2cbf789bc5f4fabca7ab036856304a658d0.

--- PrePi/Pi.c
+++ PrePi/Pi.c
@@ -1,7 +1,6 @@
 // Pi.c: Entry point for SEC(Security).
 
 #include "Pi.h"
-#include "HvcPatch.h"
 
 #include <Pi/PiBootMode.h>
 #include <Pi/PiHob.h>
@@ -27,48 +26,7 @@
 #include <Library/PlatformPrePiLib.h>
 #include <Library/SerialPortLib.h>
 
-#include <IndustryStandard/ArmStdSmc.h>
-#include <Library/ArmHvcLib.h>
-#include <Library/ArmSmcLib.h>
-
 VOID EFIAPI ProcessLibraryConstructorList(VOID);
-
-STATIC VOID PsciFixupInit(VOID)
-{
-  EFI_PHYSICAL_ADDRESS WakeFromPowerGatePatchOffset;
-  EFI_PHYSICAL_ADDRESS LowerELSynchronous64PatchOffset;
-  EFI_PHYSICAL_ADDRESS LowerELSynchronous32PatchOffset;
-  ARM_HVC_ARGS         StubArgsHvc;
-  ARM_SMC_ARGS         StubArgsSmc;
-
-  WakeFromPowerGatePatchOffset    = WAKE_FROM_POWERGATE_PATCH_ADDR;
-  LowerELSynchronous64PatchOffset = LOWER_EL_SYNC_EXC_64B_PATCH_ADDR;
-  LowerELSynchronous32PatchOffset = LOWER_EL_SYNC_EXC_32B_PATCH_ADDR;
-
-  CopyMem(
-      (VOID *)WakeFromPowerGatePatchOffset, WakeFromPowerGatePatchHandler,
-      sizeof(WakeFromPowerGatePatchHandler));
-
-  CopyMem(
-      (VOID *)LowerELSynchronous64PatchOffset, LowerELSynchronous64PatchHandler,
-      sizeof(LowerELSynchronous64PatchHandler));
-
-  CopyMem(
-      (VOID *)LowerELSynchronous32PatchOffset, LowerELSynchronous32PatchHandler,
-      sizeof(LowerELSynchronous32PatchHandler));
-
-  ArmDataSynchronizationBarrier();
-  ArmInvalidateDataCache();
-  ArmInvalidateInstructionCache();
-
-  // Call into the handler to make HCR_EL2.TSC sticky
-  StubArgsHvc.Arg0 = ARM_SMC_ID_PSCI_VERSION;
-  ArmCallHvc(&StubArgsHvc);
-
-  // Well...
-  StubArgsSmc.Arg0 = ARM_SMC_ID_PSCI_VERSION;
-  ArmCallSmc(&StubArgsSmc);
-}
 
 VOID PrePiMain(IN VOID *StackBase, IN UINTN StackSize)
 {
@@ -80,9 +38,6 @@ VOID PrePiMain(IN VOID *StackBase, IN UINTN StackSize)
   UINTN MemorySize       = 0;
   UINTN UefiMemoryBase   = 0;
   UINTN UefiMemorySize   = 0;
-
-  // PSCI fixup init
-  PsciFixupInit();
 
   // Architecture-specific initialization
   // Enable Floating Point
