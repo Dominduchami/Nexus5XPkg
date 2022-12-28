bc77d9f6d91de0c71f644ea54e7961e55c5f6576
Dominik Kobinski <dominikkobinski314@gmail.com>
2022-12-28 13:25:48 +0100

    Cleanup

--- build_uefi_talkman.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-stuart_setup -c Platforms/Nexus5XPkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
-stuart_update -c Platforms/Nexus5XPkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
-stuart_build -c Platforms/Nexus5XPkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
