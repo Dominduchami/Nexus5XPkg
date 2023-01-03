589b40f71d578552d529a127ae280ea46dee3812
Dominik Kobinski <dominikkobinski314@gmail.com>
2023-01-03 22:00:10 +0100

    PlatformUtils: correct-ish stride

--- Silicon/QC/Msm8994/QcomPkg/Library/PlatformPrePiLib/PlatformUtils.c
+++ Silicon/QC/Msm8994/QcomPkg/Library/PlatformPrePiLib/PlatformUtils.c
@@ -18,12 +18,12 @@ BOOLEAN IsLinuxBootRequested(VOID)
 
 VOID CheckMdpConfig(VOID)
 {
-	uint32_t tmp_stride = readl(PIPE_BASE + PIPE_SSPP_SRC_YSTRIDE);
+	//uint32_t tmp_stride = readl(PIPE_BASE + PIPE_SSPP_SRC_YSTRIDE);
 
   	/* Windows requires a BGRA FB */
   	writel(0x000236FF, PIPE_BASE + PIPE_SSPP_SRC_FORMAT);
   	writel(0x03020001, PIPE_BASE + PIPE_SSPP_SRC_UNPACK_PATTERN);
-  	writel(tmp_stride/4, PIPE_BASE + PIPE_SSPP_SRC_YSTRIDE);
+  	writel(1440*4, PIPE_BASE + PIPE_SSPP_SRC_YSTRIDE);
   	writel(BIT(3), MDP_CTL_0_BASE + CTL_FLUSH);
 }
 
