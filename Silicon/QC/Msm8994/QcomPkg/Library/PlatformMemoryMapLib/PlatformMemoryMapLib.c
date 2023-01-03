6dbd94bd6440b42ae913860cce4ab277042e81fe
Dominik Kobinski <dominikkobinski314@gmail.com>
2023-01-03 20:21:20 +0100

    Memory: remove ram partitions for now

--- Silicon/QC/Msm8994/QcomPkg/Library/PlatformMemoryMapLib/PlatformMemoryMapLib.c
+++ Silicon/QC/Msm8994/QcomPkg/Library/PlatformMemoryMapLib/PlatformMemoryMapLib.c
@@ -63,15 +63,15 @@ static ARM_MEMORY_REGION_DESCRIPTOR_EX gDeviceMemoryDescriptorEx[] = {
     {"HLOS 3",            0x0F200000, 0x10E00000, AddMem, SYS_MEM, SYS_MEM_CAP,  Conv,   WRITE_BACK},
 #endif
 
-    /* RAM partition regions */
+    /* RAM partition regions 
 #if MEMORY_3GB == 1
     {"RAM Partition",     0x20000000, 0x40000000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
     {"RAM Partition",     0x80000000, 0x2C1C0000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
-    /* reserved mem: 0xac1c0000 0x0000000000001000;*/
+    /* reserved mem: 0xac1c0000 0x0000000000001000;
     {"RAM Partition",     0xAC1C1000, 0x33E3F000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
 #else
     {"RAM Partition",     0x20000000, 0x60000000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
-#endif
+#endif*/
 
     /* Other memory regions */
     {"IMEM SMEM Base",    0xFE805000, 0x00001000, NoHob,  MMAP_IO, INITIALIZED,  Conv,   NS_DEVICE},
