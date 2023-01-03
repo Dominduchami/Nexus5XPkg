5eae9971423b03987fab97eb0765fc315c356860
Dominik Kobinski <dominikkobinski314@gmail.com>
2023-01-03 21:03:14 +0100

    PlatformUtils: stride

--- Silicon/QC/Msm8994/QcomPkg/Library/PlatformPrePiLib/PlatformUtils.c
+++ Silicon/QC/Msm8994/QcomPkg/Library/PlatformPrePiLib/PlatformUtils.c
@@ -23,7 +23,7 @@ VOID CheckMdpConfig(VOID)
   	/* Windows requires a BGRA FB */
   	writel(0x000236FF, PIPE_BASE + PIPE_SSPP_SRC_FORMAT);
   	writel(0x03020001, PIPE_BASE + PIPE_SSPP_SRC_UNPACK_PATTERN);
-  	writel(tmp_stride*4, PIPE_BASE + PIPE_SSPP_SRC_YSTRIDE);
+  	writel(tmp_stride/4, PIPE_BASE + PIPE_SSPP_SRC_YSTRIDE);
   	writel(BIT(3), MDP_CTL_0_BASE + CTL_FLUSH);
 }
 
