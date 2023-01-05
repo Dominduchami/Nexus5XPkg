1b8f63b92a933870be0c013fcaa10654bd35ba65
Dominik Kobinski <dominikkobinski314@gmail.com>
2023-01-05 19:49:08 +0100

    PlatformUtils: disable fb reconfig

--- Silicon/QC/Msm8994/QcomPkg/Library/PlatformPrePiLib/PlatformUtils.c
+++ Silicon/QC/Msm8994/QcomPkg/Library/PlatformPrePiLib/PlatformUtils.c
@@ -16,18 +16,15 @@ BOOLEAN IsLinuxBootRequested(VOID)
   return FALSE;
 }
 
-VOID CheckMdpConfig(VOID)
+/*VOID CheckMdpConfig(VOID)
 {
-  /* Windows requires a BGRA FB */
   MmioWrite32(PIPE_BASE + PIPE_SSPP_SRC_FORMAT, 0x000236FF);
   MmioWrite32(PIPE_BASE + PIPE_SSPP_SRC_UNPACK_PATTERN, 0x03020001);
 #if SILICON_PLATFORM == 8992
   MmioWrite32(PIPE_BASE + PIPE_SSPP_SRC_YSTRIDE, 1080*4);
-#else
-  MmioWrite32(PIPE_BASE + PIPE_SSPP_SRC_YSTRIDE, 720*4);
 #endif
   MmioWrite32(MDP_CTL_0_BASE + CTL_FLUSH, (1 << (3)));
-}
+}*/
 
 STATIC
 VOID
@@ -54,5 +51,5 @@ VOID PlatformInitialize(VOID)
 {
   DisplayEnableRefresh();
 
-  CheckMdpConfig();
+  //CheckMdpConfig();
 }
