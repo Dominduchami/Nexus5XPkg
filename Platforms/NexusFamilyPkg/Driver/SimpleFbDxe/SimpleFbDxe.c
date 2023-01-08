3c48719934e87fa8185c7a6d4913039ce30c29ea
Dominik Kobinski <dominikkobinski314@gmail.com>
2023-01-08 14:25:07 +0100

    SimpleFbDxe: more experimental bpp24 changes

--- Platforms/NexusFamilyPkg/Driver/SimpleFbDxe/SimpleFbDxe.c
+++ Platforms/NexusFamilyPkg/Driver/SimpleFbDxe/SimpleFbDxe.c
@@ -28,11 +28,8 @@
   ((UINT8                                                                      \
         *)((UINTN)This->Mode->FrameBufferBase + (posY)*This->Mode->Info->PixelsPerScanLine * FB_BYTES_PER_PIXEL + (posX)*FB_BYTES_PER_PIXEL))
 
-#if SILICON_PLATFORM == 8992
-#define FB_BITS_PER_PIXEL (32)
-#else
-#define FB_BITS_PER_PIXEL (24)
-#endif
+
+#define FB_BITS_PER_PIXEL (24)//32
 #define FB_BYTES_PER_PIXEL (FB_BITS_PER_PIXEL / 8)
 #define DISPLAYDXE_PHYSICALADDRESS32(_x_) (UINTN)((_x_)&0xFFFFFFFF)
 
@@ -51,6 +48,7 @@ enum video_log2_bpp {
   VIDEO_BPP4,
   VIDEO_BPP8,
   VIDEO_BPP16,
+  VIDEO_BPP24,
   VIDEO_BPP32,
 };
 
@@ -221,7 +219,7 @@ SimpleFbDxeInitialize(
   mDisplay.Mode->Info->VerticalResolution   = MipiFrameBufferHeight;
 
   /* SimpleFB runs on a8r8g8b8 (VIDEO_BPP32) for WoA devices */
-  UINT32               LineLength = MipiFrameBufferWidth * VNBYTES(VIDEO_BPP32);
+  UINT32               LineLength = MipiFrameBufferWidth * VNBYTES(VIDEO_BPP24);
   UINT32               FrameBufferSize    = LineLength * MipiFrameBufferHeight;
   EFI_PHYSICAL_ADDRESS FrameBufferAddress = MipiFrameBufferAddr;
 
