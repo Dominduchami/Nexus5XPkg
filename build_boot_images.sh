df3ef6a054f959a074c0a464f91760a2164611f3
Dominduchami <dominikkobinski314@gmail.com>
2023-01-15 18:23:38 +0100

    ImageResources: Drop stock fdts

--- build_boot_images.sh
+++ build_boot_images.sh
@@ -4,7 +4,9 @@ cat ./BootShim/BootShim.bin ./Build/Nexus5X-AARCH64/DEBUG_CLANG38/FV/MSM8992_EFI
 
 gzip -c < ./ImageResources/Bullhead/bootpayload.bin >./ImageResources/Bullhead/bootpayload.bin.gz
 
-cat ./ImageResources/Bullhead/bootpayload.bin.gz ./ImageResources/Bullhead/fdt.img >> ./ImageResources/Bullhead/Image.gz-dtb
+dtc -I dts -O dtb ./ImageResources/Bullhead/bullhead.dts -o ./ImageResources/Bullhead/bullhead.dtb
+
+cat ./ImageResources/Bullhead/bootpayload.bin.gz ./ImageResources/Bullhead/bullhead.dtb >> ./ImageResources/Bullhead/Image.gz-dtb
 
 mkbootimg --kernel ./ImageResources/Bullhead/Image.gz-dtb \
   --ramdisk ./ImageResources/ramdisk-null \
@@ -18,7 +20,9 @@ cat ./BootShim/BootShim.bin ./Build/Nexus6P-AARCH64/DEBUG_CLANG38/FV/MSM8994_EFI
 
 gzip -c < ./ImageResources/Angler/bootpayload.bin >./ImageResources/Angler/bootpayload.bin.gz
 
-cat ./ImageResources/Angler/bootpayload.bin.gz ./ImageResources/Angler/fdt.img >> ./ImageResources/Angler/Image.gz-dtb
+dtc -I dts -O dtb ./ImageResources/Angler/angler.dts -o ./ImageResources/Angler/angler.dtb
+
+cat ./ImageResources/Angler/bootpayload.bin.gz ./ImageResources/Angler/angler.dtb >> ./ImageResources/Angler/Image.gz-dtb
 
 mkbootimg --kernel ./ImageResources/Angler/Image.gz-dtb \
   --ramdisk ./ImageResources/ramdisk-null \
