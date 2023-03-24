ea803f6c40786e289af9390c02a31694cbeed437
Dominduchami <dominikkobinski314@gmail.com>
2023-03-24 22:30:43 +0100

    Memory: Angler: Move DXE Heap (again)

--- Silicon/QC/Msm8994/QcomPkg/Library/PlatformMemoryMapLib/PlatformMemoryMapLib.c
+++ Silicon/QC/Msm8994/QcomPkg/Library/PlatformMemoryMapLib/PlatformMemoryMapLib.c
@@ -55,7 +55,9 @@ static ARM_MEMORY_REGION_DESCRIPTOR_EX gDeviceMemoryDescriptorEx[] = {
     {"MPSS_EFS / SBL",    0x06E80000, 0x00180000, AddMem, SYS_MEM, SYS_MEM_CAP,  Reserv, NS_DEVICE},
     {"Subsy Res. 1/DHMS", 0x07000000, 0x07F00000, AddMem, SYS_MEM, SYS_MEM_CAP,  Reserv, NS_DEVICE},
     {"CNSS_DEBUG",        0x0EF00000, 0x00300000, AddMem, SYS_MEM, SYS_MEM_CAP,  Reserv, NS_DEVICE},
-    {"DXE Heap",          0x0F200000, 0x10E00000, AddMem, SYS_MEM, SYS_MEM_CAP,  Conv,   WRITE_BACK_XN},
+    {"Reserved",          0x0F200000, 0x00200000, AddMem, SYS_MEM, SYS_MEM_CAP,  Reserv, NS_DEVICE},
+    {"DXE Heap",          0x0F400000, 0x07800000, AddMem, SYS_MEM, SYS_MEM_CAP,  Conv,   WRITE_BACK_XN},
+    {"HLOS 4",            0x16A00000, 0x09600000, AddMem, SYS_MEM, SYS_MEM_CAP,  Conv,   WRITE_BACK},
 #endif
     /* DDR Bank 0 end */
 
