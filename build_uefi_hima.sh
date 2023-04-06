943e88de2806eb1bcb8e04502e5b1d6ed11e7f2c
Dominik Kobinski <dominikkobinski314@gmail.com>
2023-04-06 10:34:44 +0200

    build_uefi.sh: Correct a typo

--- build_uefi_hima.sh
+++ build_uefi_hima.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 stuart_setup -c Platforms/HtcOneM9Pkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
-stuart_update -c Platforms/HtcOneM9PPkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
+stuart_update -c Platforms/HtcOneM9Pkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
 stuart_build -c Platforms/HtcOneM9Pkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
