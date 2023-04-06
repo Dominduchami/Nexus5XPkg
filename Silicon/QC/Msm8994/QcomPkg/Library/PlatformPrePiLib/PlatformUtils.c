54ae920d37e06b91a955081249d9ad6593f56214
Dominik Kobinski <dominikkobinski314@gmail.com>
2023-04-06 09:13:41 +0200

    PlatformUtils: Disable nexus-specific functions for now

--- Silicon/QC/Msm8994/QcomPkg/Library/PlatformPrePiLib/PlatformUtils.c
+++ Silicon/QC/Msm8994/QcomPkg/Library/PlatformPrePiLib/PlatformUtils.c
@@ -29,7 +29,7 @@ VOID CheckMdpConfig(VOID)
   MmioWrite32(MDP_CTL_0_BASE + CTL_FLUSH, (1 << (3)));
 #else
   /* Angler uses dual pipe framebuffer */
-  MmioWrite32(ANGLER_PIPE_BASE + PIPE_SSPP_SRC_FORMAT, 0x000237FF);
+  /*MmioWrite32(ANGLER_PIPE_BASE + PIPE_SSPP_SRC_FORMAT, 0x000237FF);
   MmioWrite32(ANGLER_PIPE_BASE + PIPE_SSPP_SRC_UNPACK_PATTERN, 0x03020001);
   MmioWrite32(ANGLER_PIPE_BASE + PIPE_SSPP_SRC_YSTRIDE, 1440*4);
   MmioWrite32(MDP_CTL_0_BASE + CTL_FLUSH, (1 << (0)));
@@ -37,39 +37,11 @@ VOID CheckMdpConfig(VOID)
   MmioWrite32(ANGLER_PIPE2_BASE + PIPE_SSPP_SRC_FORMAT, 0x000237FF);
   MmioWrite32(ANGLER_PIPE2_BASE + PIPE_SSPP_SRC_UNPACK_PATTERN, 0x03020001);
   MmioWrite32(ANGLER_PIPE2_BASE + PIPE_SSPP_SRC_YSTRIDE, 1440*4);
-  MmioWrite32(MDP_CTL_1_BASE + CTL_FLUSH, (1 << (1)));
+  MmioWrite32(MDP_CTL_1_BASE + CTL_FLUSH, (1 << (1)));*/
 #endif
-}
-
-STATIC
-VOID
-DisplayEnableRefresh(VOID)
-{
-#if SILICON_PLATFORM == 8994
-  uint32_t height = FixedPcdGet32(PcdMipiFrameBufferHeight);
-  uint32_t vsync_count = 19200000 / (height * 60);
-#endif
-  uint32_t mdss_mdp_rev = readl(MDP_HW_REV);
-  uint32_t pp0_base;
-
-  if (mdss_mdp_rev >= MDSS_MDP_HW_REV_105)
-    pp0_base = REG_MDP(0x71000);
-  else if (mdss_mdp_rev >= MDSS_MDP_HW_REV_102)
-    pp0_base = REG_MDP(0x12D00);
-  else
-    pp0_base = REG_MDP(0x21B00);
-
-#if SILICON_PLATFORM == 8994
-  /* Required for working framebuffer on angler, breaks on bullhead */
-  MmioWrite32(pp0_base + MDP_PP_SYNC_CONFIG_VSYNC, vsync_count | BIT(19));
-#endif
-  MmioWrite32(pp0_base + MDP_PP_AUTOREFRESH_CONFIG, BIT(31) | 1);
-  MmioWrite32(MDP_CTL_0_BASE + CTL_START, 1);
 }
 
 VOID PlatformInitialize(VOID)
 {
-  DisplayEnableRefresh();
-
   CheckMdpConfig();
 }
