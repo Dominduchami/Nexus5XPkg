6b5edcb7f1632e99b208de1984c2457145992de1
Dominduchami <dominikkobinski314@gmail.com>
2023-10-25 21:43:30 +0200

    PlatformUtils: Cleanup and documentation

--- Silicon/QC/Msm8994/QcomPkg/Library/PlatformPrePiLib/PlatformUtils.c
+++ Silicon/QC/Msm8994/QcomPkg/Library/PlatformPrePiLib/PlatformUtils.c
@@ -19,57 +19,72 @@ BOOLEAN IsLinuxBootRequested(VOID)
   return FALSE;
 }
 
+/* 
+ * Reconfigure the framebuffer to a BGRA 32bpp one (windows requirements)
+ *
+ * Uses code from https://imbushuo.net/blog/archives/590/ (Little-kernel section)
+ * 
+ * TODO: Dynamically detect pipes
+ */
 VOID CheckMdpConfig(VOID)
 {
-  /* Windows requires a BGRA FB */
+  UINT32 Width = FixedPcdGet32(PcdMipiFrameBufferWidth);
+
 #if SILICON_PLATFORM == 8992
   MmioWrite32(BULLHEAD_PIPE_BASE + PIPE_SSPP_SRC_FORMAT, 0x000236FF);
   MmioWrite32(BULLHEAD_PIPE_BASE + PIPE_SSPP_SRC_UNPACK_PATTERN, 0x03020001);
-  MmioWrite32(BULLHEAD_PIPE_BASE + PIPE_SSPP_SRC_YSTRIDE, 1080*4);
+  MmioWrite32(BULLHEAD_PIPE_BASE + PIPE_SSPP_SRC_YSTRIDE, Width * 4);
   MmioWrite32(MDP_CTL_0_BASE + CTL_FLUSH, (1 << (3)));
 #else
-  /* Angler uses dual pipe framebuffer */
+  /* Angler uses dual-pipe framebuffer */
   MmioWrite32(ANGLER_PIPE_BASE + PIPE_SSPP_SRC_FORMAT, 0x000237FF);
   MmioWrite32(ANGLER_PIPE_BASE + PIPE_SSPP_SRC_UNPACK_PATTERN, 0x03020001);
-  MmioWrite32(ANGLER_PIPE_BASE + PIPE_SSPP_SRC_YSTRIDE, 1440*4);
+  MmioWrite32(ANGLER_PIPE_BASE + PIPE_SSPP_SRC_YSTRIDE, Width * 4);
   MmioWrite32(MDP_CTL_0_BASE + CTL_FLUSH, (1 << (0)));
 
   MmioWrite32(ANGLER_PIPE2_BASE + PIPE_SSPP_SRC_FORMAT, 0x000237FF);
   MmioWrite32(ANGLER_PIPE2_BASE + PIPE_SSPP_SRC_UNPACK_PATTERN, 0x03020001);
-  MmioWrite32(ANGLER_PIPE2_BASE + PIPE_SSPP_SRC_YSTRIDE, 1440*4);
+  MmioWrite32(ANGLER_PIPE2_BASE + PIPE_SSPP_SRC_YSTRIDE, Width * 4);
   MmioWrite32(MDP_CTL_1_BASE + CTL_FLUSH, (1 << (1)));
 #endif
 }
 
+/* 
+ * Turn on autorefresh on command-mode panels 
+ * (stock bootloader disables it here).
+ *
+ * Based on lk2nd code :
+ * https://github.com/msm8916-mainline/lk2nd/pull/137/commits/d0492fdd1f5e6ff51a181fd6d4920c62adc1a23e#diff-c75b4efb45794d329b2414b12f0a97190c5fe0d9dd43f33e84969e1c34f5ca99R158
+ */
 STATIC
 VOID
 DisplayEnableRefresh(VOID)
 {
 #if SILICON_PLATFORM == 8994
-  uint32_t height = FixedPcdGet32(PcdMipiFrameBufferHeight);
-  uint32_t vsync_count = 19200000 / (height * 60);
+  UINT32 Height = FixedPcdGet32(PcdMipiFrameBufferHeight);
+  UINT32 VsyncCount = 19200000 / (Height * 60);
 #endif
-  uint32_t mdss_mdp_rev = readl(MDP_HW_REV);
-  uint32_t pp0_base;
+  UINT32 MdssMdpRev = MmioRead32(MDP_HW_REV);
+  UINT32 pp0_base;
 
-  if (mdss_mdp_rev >= MDSS_MDP_HW_REV_105)
+  if (MdssMdpRev >= MDSS_MDP_HW_REV_105)
     pp0_base = REG_MDP(0x71000);
-  else if (mdss_mdp_rev >= MDSS_MDP_HW_REV_102)
+  else if (MdssMdpRev >= MDSS_MDP_HW_REV_102)
     pp0_base = REG_MDP(0x12D00);
   else
     pp0_base = REG_MDP(0x21B00);
 
 #if SILICON_PLATFORM == 8994
-  /* Required for working framebuffer on angler, breaks on bullhead */
-  MmioWrite32(pp0_base + MDP_PP_SYNC_CONFIG_VSYNC, vsync_count | BIT(19));
+  /* Vsync write required for working framebuffer on angler, breaks on bullhead */
+  MmioWrite32(pp0_base + MDP_PP_SYNC_CONFIG_VSYNC, VsyncCount | BIT(19));
 #endif
-  MmioWrite32(pp0_base + MDP_PP_AUTOREFRESH_CONFIG, BIT(31) | 1);
+  /* Enable autorefresh and flush */
+  MmioWrite32(pp0_base + MDP_PP_AUTOREFRESH_CONFIG, BIT(31) | AUTOREFRESH_FRAMENUM);
   MmioWrite32(MDP_CTL_0_BASE + CTL_START, 1);
 }
 
 VOID PlatformInitialize(VOID)
 {
   DisplayEnableRefresh();
-
   CheckMdpConfig();
 }
